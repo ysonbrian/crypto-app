c995e26acd83d0f2d8e6e4ff7f2dca2dd10bf203
ysonbrian <znlsakxls@gmail.com>
2021-10-02 01:01:06 +0900

    Homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Title } = Typography;
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total cryptocurrencies" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchages" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24th Volume" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value="5" />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Homepage;
